95587f3e4b1c6244b04b605d4c93a4a886336cf7
tripod31 <tripod31@hotmail.com>
2015-04-24 16:01:53 +0900

    upd

--- common.php
+++ common.php
@@ -81,12 +81,12 @@
                 throw new Exception(getErrorMsg($conn));
             if ($conn->exec("DELETE from installed")===FALSE)
                 throw new Exception(getErrorMsg($conn));
-            if (!file_exists("/usr/bin/rpm"))
-                throw new Exception("/usr/bin/rpmがない");
+            if (!file_exists("/bin/rpm")&&!file_exists("/usr/bin/rpm"))
+                throw new Exception("rpmがない");
             $pp = popen("rpm -qa","r");
             if ($pp === FALSE)
                 throw new Exception("rpm -qaでエラー");
-            $this->read_packeages($conn,$pp,'installed');
+            $this->read_packages($conn,$pp,'installed');
             pclose($pp);
                     
             #読み取り時刻
@@ -105,8 +105,8 @@
             if ($conn->exec("DELETE from installed_org")===FALSE)
                 throw new Exception(getErrorMsg($conn));
             if ($fp==null){
-                if (!file_exists("/usr/bin/rpm"))
-                    throw new Exception("/usr/bin/rpmがない");
+                if (!file_exists("/usr/bin/rpm")&&!file_exists("/bin/rpm"))
+                    throw new Exception("rpmがない");
                 $fp = popen("rpm -qa","r");
                 if ($fp === FALSE)
                     throw new Exception("rpm -qaでエラー");
@@ -126,19 +126,26 @@
             $lno = 0;
             while ($package = fgets($p_fp)){
                 $lno++;
-                $fp = popen("rpm -qp " . $package,"r");
+                #if ($package == "gpg-pubkey")
+                #    continue;
+                $fp = popen(" rpm --queryformat '%{NAME}__TAB__%{VERSION}__TAB__%{DESCRIPTION}' -q " . $package,"r");
                 if ($fp === FALSE)
-                    throw new Exception("rpm -qpでエラー");
-                while ($line = fgets($fp)){
-                    $res = preg_match("/(\S+)\s+(\S+)\s+(\S+)(.+)/",$line,$arr);
-                    if ($res){
-                        $sql = sprintf("INSERT INTO %s values('%s','%s','%s','%s')",$tbl,$arr[1],$arr[2],$arr[3],str_replace("'","",$arr[4]));
+                    throw new Exception("rpm -qでエラー");
+                $all_line="";
+                while($line = fgets($fp)){
+                    $all_line.=str_replace("\n","",$line);
+                }
+                $res = preg_match("/(.+)__TAB__(.+)__TAB__(.+)/",$all_line,$arr);
+                if ($res){
+                    $name =$arr[1];
+                    if ($name !="gpg-pubkey"){
+                        $sql = sprintf("INSERT INTO %s values('%s','%s','%s','%s')",$tbl,'installed',$name,$arr[2],str_replace("'","",$arr[3]));
                         #print_r ($sql);
                         if ( $conn->exec($sql)===FALSE)
                             throw new Exception(getErrorMsg($conn));
-                    } else {
-                        print "rpm -qp:フォーマットが変<BR>" . $line . "<BR>";
                     }
+                } else {
+                    print "rpm -q:フォーマットが変<BR>" . $all_line . "<BR>";
                 }
             }
         }
