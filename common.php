593cf7cd65ef0107247fe5538ffd51bacb6fce23
tripod31 <tripod31@hotmail.com>
2015-04-24 18:53:24 +0900

    abstract!

--- common.php
+++ common.php
@@ -1,34 +1,29 @@
 <?php
     define("DBFILE","sqlite:./dpkg.db");
-    interface IImport{
-        public function imp_installed();
-        public function imp_installed_org($fp=null);
-    }
     
-    class import_dpkg implements IImport{
+    abstract class import {
+        abstract private function open_list_command();
+        abstract private function read_package($conn,$tbl,$line,$lno);
+        
         public function imp_installed(){
             $conn=new PDO(DBFILE,null,null);
             if ($conn->beginTransaction()===FALSE)
                 throw new Exception(getErrorMsg($conn));
             if ($conn->exec("DELETE from installed")===FALSE)
                 throw new Exception(getErrorMsg($conn));
-            if (!file_exists("/usr/bin/dpkg"))
-                throw new Exception("/usr/bin/dpkgがない");
-            $pp = popen("dpkg -l","r");
-            if ($pp === FALSE)
-                throw new Exception("dpkg -lでエラー");
+            $pp = open_list_command();
             $this->read_packages($conn,$pp,'installed');
             pclose($pp);
-                    
+    
             #読み取り時刻
             $sql=sprintf("UPDATE info SET info='%s' WHERE name='saved_time_cur'",date( "Y/m/d H:i:s", time()));
             if( $conn->exec($sql)===FALSE)
-            throw new Exception(getErrorMsg($conn));
+                throw new Exception(getErrorMsg($conn));
     
             if($conn->commit()===FALSE)
-            throw new Exception(getErrorMsg($conn));
+                throw new Exception(getErrorMsg($conn));
         }
-        
+    
         public function imp_installed_org($fp=null){
             $conn=new PDO(DBFILE,null,null);
             if ($conn->beginTransaction()===FALSE)
@@ -36,117 +31,85 @@
             if ($conn->exec("DELETE from installed_org")===FALSE)
                 throw new Exception(getErrorMsg($conn));
             if ($fp==null){
-                if (!file_exists("/usr/bin/dpkg"))
-                    throw new Exception("/usr/bin/dpkgがない");
-                $fp = popen("dpkg -l","r");
-                if ($fp === FALSE)
-                    throw new Exception("dpkg -lでエラー");
+                $fp=open_list_command();
             }
             $this->read_packages($conn,$fp,'installed_org');
-            
+    
             #読み取り時刻
             $sql=sprintf("UPDATE info SET info='%s' WHERE name='saved_time_org'",date( "Y/m/d H:i:s", time()));
             if( $conn->exec($sql)===FALSE)
                 throw new Exception(getErrorMsg($conn));
-            
+    
             if ($conn->commit()===FALSE)
                 throw new Exception(getErrorMsg($conn));
         }
-        
+                
         private function read_packages($conn,$fp,$tbl){
             $lno = 0;
             while ($line = fgets($fp)){
                 $lno++;
-                #ヘッダスキップ
-                if ($lno <= 5) {
-                    continue;            
-                }
-                $res = preg_match("/(\S+)\s+(\S+)\s+(\S+)(.+)/",$line,$arr);
-                if ($res){
-                    $sql = sprintf("INSERT INTO %s values('%s','%s','%s','%s')",$tbl,$arr[1],$arr[2],$arr[3],str_replace("'","",$arr[4]));
-                    #print_r ($sql);
-                    if ( $conn->exec($sql)===FALSE)
-                        throw new Exception(getErrorMsg($conn));
-                } else {
-                    print "dpkg-l:フォーマットが変<BR>" . $line . "<BR>";
-                }
+                read_package($conn,$tbl,$line,$lno);
             }
         }
     }
     
-    class import_rpm implements IImport{
-        public function imp_installed(){
-            $conn=new PDO(DBFILE,null,null);
-            if ($conn->beginTransaction()===FALSE)
-                throw new Exception(getErrorMsg($conn));
-            if ($conn->exec("DELETE from installed")===FALSE)
-                throw new Exception(getErrorMsg($conn));
+    class import_dpkg extends import{
+        private function open_list_command(){
+            if (!file_exists("/usr/bin/dpkg"))
+                throw new Exception("/usr/bin/dpkgがない");      
+            $pp = popen("dpkg -l","r");
+            if ($pp === FALSE)
+                throw new Exception("dpkg -lでエラー");
+            return $pp;            
+        }
+        
+        private function read_package($conn,$tbl,$line,$lno){
+            if ($lno <= 5)
+                return; #ヘッダスキップ
+            
+            $res = preg_match("/(\S+)\s+(\S+)\s+(\S+)(.+)/",$line,$arr);
+            if ($res){
+                $sql = sprintf("INSERT INTO %s values('%s','%s','%s','%s')",$tbl,$arr[1],$arr[2],$arr[3],str_replace("'","",$arr[4]));
+                #print_r ($sql);
+                if ( $conn->exec($sql)===FALSE)
+                    throw new Exception(getErrorMsg($conn));
+            } else {
+                print "dpkg-l:フォーマットが変<BR>" . $line . "<BR>";
+            }        
+        }       
+     }
+    
+    class import_rpm extends import{
+        private function open_list_command(){
             if (!file_exists("/bin/rpm")&&!file_exists("/usr/bin/rpm"))
                 throw new Exception("rpmがない");
             $pp = popen("rpm -qa","r");
             if ($pp === FALSE)
                 throw new Exception("rpm -qaでエラー");
-            $this->read_packages($conn,$pp,'installed');
-            pclose($pp);
-                    
-            #読み取り時刻
-            $sql=sprintf("UPDATE info SET info='%s' WHERE name='saved_time_cur'",date( "Y/m/d H:i:s", time()));
-            if( $conn->exec($sql)===FALSE)
-            throw new Exception(getErrorMsg($conn));
-    
-            if($conn->commit()===FALSE)
-            throw new Exception(getErrorMsg($conn));
+            return $pp;        
         }
         
-        public function imp_installed_org($fp=null){
-            $conn=new PDO(DBFILE,null,null);
-            if ($conn->beginTransaction()===FALSE)
-                throw new Exception(getErrorMsg($conn));
-            if ($conn->exec("DELETE from installed_org")===FALSE)
-                throw new Exception(getErrorMsg($conn));
-            if ($fp==null){
-                if (!file_exists("/usr/bin/rpm")&&!file_exists("/bin/rpm"))
-                    throw new Exception("rpmがない");
-                $fp = popen("rpm -qa","r");
-                if ($fp === FALSE)
-                    throw new Exception("rpm -qaでエラー");
+        private function read_package($conn,$tbl,$package,$lno){
+            $fp = popen(" rpm --queryformat '%{NAME}__TAB__%{VERSION}__TAB__%{DESCRIPTION}' -q " . $package,"r");
+            if ($fp === FALSE)
+                throw new Exception("rpm -qでエラー");
+            $all_line="";
+            while($line = fgets($fp)){
+                $all_line.=str_replace("\n","",$line);
             }
-            $this->read_packages($conn,$fp,'installed_org');
-            
-            #読み取り時刻
-            $sql=sprintf("UPDATE info SET info='%s' WHERE name='saved_time_org'",date( "Y/m/d H:i:s", time()));
-            if( $conn->exec($sql)===FALSE)
-                throw new Exception(getErrorMsg($conn));
-            
-            if ($conn->commit()===FALSE)
-                throw new Exception(getErrorMsg($conn));
-        }
-        
-        private function read_packages($conn,$p_fp,$tbl){
-            $lno = 0;
-            while ($package = fgets($p_fp)){
-                $lno++;
-                $fp = popen(" rpm --queryformat '%{NAME}__TAB__%{VERSION}__TAB__%{DESCRIPTION}' -q " . $package,"r");
-                if ($fp === FALSE)
-                    throw new Exception("rpm -qでエラー");
-                $all_line="";
-                while($line = fgets($fp)){
-                    $all_line.=str_replace("\n","",$line);
+            $res = preg_match("/(.+)__TAB__(.+)__TAB__(.+)/",$all_line,$arr);
+            if ($res){
+                $name =$arr[1];
+                if ($name !="gpg-pubkey"){
+                    $sql = sprintf("INSERT INTO %s values('%s','%s','%s','%s')",$tbl,'installed',$name,$arr[2],str_replace("'","",$arr[3]));
+                    #print_r ($sql);
+                    if ( $conn->exec($sql)===FALSE)
+                        throw new Exception(getErrorMsg($conn));
                 }
-                $res = preg_match("/(.+)__TAB__(.+)__TAB__(.+)/",$all_line,$arr);
-                if ($res){
-                    $name =$arr[1];
-                    if ($name !="gpg-pubkey"){
-                        $sql = sprintf("INSERT INTO %s values('%s','%s','%s','%s')",$tbl,'installed',$name,$arr[2],str_replace("'","",$arr[3]));
-                        #print_r ($sql);
-                        if ( $conn->exec($sql)===FALSE)
-                            throw new Exception(getErrorMsg($conn));
-                    }
-                } else {
-                    print "rpm -q:フォーマットが変<BR>" . $all_line . "<BR>";
-                }
-            }
-        }
+            } else {
+                print "rpm -q:フォーマットが変<BR>" . $all_line . "<BR>";
+            }            
+        }               
     }
        
     #ファクトリメソッド
