77ab3b7503ba10e9ff1c00ba5a7e0aa08eb3f62d
tripod31 <tripod31@hotmail.com>
2015-10-11 08:57:35 +0900

    upd

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ if [ -f dpkg.db ]; then
   rm dpkg.db
 fi
 sqlite3 dpkg.db < create.sql
-sudo chgrp apache . templates_c dpkg.db
 chmod g+w . templates_c dpkg.db
+sudo chgrp www-data . templates_c dpkg.db
