9cc606eeee7280a8373a52eb7c51fead66e848ec
tripod31 <tripod31@hotmail.com>
2018-03-12 22:21:43 +0900

    ignore

--- make_tar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm dpkg_all.tar.gz
-tar -zcvf dpkg_all.tar.gz -C .. dpkg --exclude=*.tpl.php --exclude=*.tar.gz
--- make_tar_dist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm dpkg.tar.gz
-tar -zcvf dpkg.tar.gz -C .. dpkg --exclude=*.tpl.php --exclude=old --exclude=.* --exclude=make*.sh --exclude=purge.sh --exclude=nbproject --exclude=*.tar.gz
