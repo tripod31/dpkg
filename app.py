68b824a7f05d52bd89d5901d5d459484f1afc3d2
tripod31 <tripod31@hotmail.com>
2024-04-14 21:31:27 +0900

    upd

--- app.py
+++ app.py
@@ -30,12 +30,28 @@ def index():
 @app.route('/ajax', methods=['POST'])
 def ajax():
     db = get_db()
+    
+    #クエリー
     sql = request.form.get('sql')
-    #print(sql)
+    pkg_name =  request.form.get('pkg_name')
+    hide_lib = request.form.get('hide_lib')
+    hide_rc = request.form.get('hide_rc')
     cur = db.execute(sql)
     colnames = [desc[0] for desc in cur.description]
-    rows = cur.fetchall()
-    
+    colname_idx = {desc[0]:i for i,desc in enumerate(cur.description)} #{列名:列番号}
+    rows_org = cur.fetchall()
+    #クエリ結果をフィルタ
+    rows =[]
+    for row in rows_org:
+        if len(pkg_name)>0 and pkg_name not in row[colname_idx["name"]]:
+            continue
+        if hide_lib and "lib" in row[colname_idx["name"]]:
+            continue
+        if hide_rc and row[colname_idx["status"]]=="rc":
+            continue
+
+        rows.append(row)
+
     cur = db.execute("SELECT * FROM info")
     info = {row[0]:row[1] for row in cur.fetchall()}  #{name:info}
 
